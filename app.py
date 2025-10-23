3597a953fd797072c694cbafa7a690e957db21ed
Mitchley <dinnermitchleyt@gmail.com>
2025-10-23 13:34:27 +0200

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.filterwarnings('ignore', category=UserWarning)
-
-import streamlit as st
-
-st.title("Diabetes Prediction Model")
-st.write("This application predicts the likelihood of diabetes readmission based on patient data.")
